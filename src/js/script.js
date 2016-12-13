1627d56cd6a8396bd5e71f507dfcb8bdc5c3b711
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-12-14 08:47:33 +0900

    construct

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,1 @@
-import Sample from './lib/Sample';
-import $ from 'jquery';
-
-const sample = new Sample({
-    name: 'world'
-});
-
-$('.wrapper').on('click', () => {
-    console.log(`hello, ${sample.name}.`);
-});
+import AnimKanpai from './module/AnimKanpai';
